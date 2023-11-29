b50d23e25323527c8a1e18bc59af367bf7b56fd2
Eliasbergman98 <elias.bergman.9452@student.uu.se>
2023-11-29 14:55:24 +0100

    Fixat style på createview

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="poll">
     <div class="gameInfo a">
-    Poll link: 
+    <!--Poll link: 
     <input type="text" v-model="pollId">
     <button v-on:click="createPoll">
       Save gameID 
-    </button> <br> <br>
-    Choose name for your quiz:
-    <input type="text" v-model="pollNameId">
+    </button> <br> <br> -->
+Choose a name for your quiz:<br>
+    <input type="text" v-model="pollNameId"> 
     <button v-on:click="addPollName">
       Add name
     </button>
     </div>
     <div class="gameInfo b">
-    <br> <br>
     Choose your avatar: <br>
     <img class="avatar">
     <button v-for="(avatar, index) in avatars" :key="index" @click="selectAvatar(index)" :class="{ 'selected': selectedAvatar === index }">
       <img class="emojies" v-bind:src="avatar.url" alt="😄" width="32" height="32">
     </button>
     </div>
+
+    <section class="button-container">
+      <router-link to="/create/"><button id="createbutton"> {{ uiLabels.createGame }}</button></router-link>
+    </section>
 
     <div class="gameInfo c">
       {{uiLabels.question}}:
@@ -106,13 +109,16 @@ export default {
 <style>
 
 .poll{
+  position: relative;
   display: grid;
-  grid-gap: 3em;
-  background-color: aqua;
+  grid-gap: 3vw;
+  background-color: rgb(163, 163, 243);
+  background-size: cover;
 }
 
 
 .gameInfo{
+  font-family: Georgia, 'Times New Roman', Times, serif;
   width: 50vw;
   height: 1vw;
   text-align: left;
@@ -120,15 +126,30 @@ export default {
 
 }
 .a{
+  padding: 10em auto 2em 2em;
+  text-align: center;
+  font-size: 2vw;
+  width: 50vw;
+  height: 5vw;
+  background-size: cover;
+  background-color: white;
+  border: 0.5em solid black;
   margin-top: 5vw;
   margin-left: 5vw;
-
 }
 
 .b{
-  width: 15vw;
+  text-align: center;
+  font-size: 2vw;
+  width: 50vw;
+  height: 10vw;
+  background-size: cover;
+  background-color: white;
+  border: 0.5em solid black;
   margin-top: 2vw;
   margin-left: 5vw;
+  padding-top: 4vw ;
+  padding-bottom: 4vw;
 }
 
 .c{
@@ -137,8 +158,28 @@ export default {
 }
 
 .selected {
-  background-color: black;
+  background-color: green;
  
+}
+
+.button-container {
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 2em;
+}
+
+#createbutton {
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  font-size: 14pt;
+  color: white;
+  background-color: green;
+  border: 2px solid black;
+  padding: 20px;
+  margin-left: 70vw;
+  border-radius: 25px;
 }
 
 </style>
