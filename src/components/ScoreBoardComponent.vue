9465a34b77de46cd71ad1e159908256d8f7f0aad
alicewik <alice.wiksten@gmail.com>
2023-12-21 18:55:00 +0100

    Fix i scoreboard

--- src/components/ScoreBoardComponent.vue
+++ src/components/ScoreBoardComponent.vue
@@ -1,25 +1,27 @@
 <template>
     <div class="score-board-component">
-        <h2> {{ uiLabels.midScoreHeading }} {{ questionNumber - 1 }}</h2>
-       <div> {{ uiLabels.rightAnswerText }} {{ Object.keys(cities)[questionNumber - 2] }} </div>
-        <p> {{ topParticipantsText }}</p>
+        <h2>{{ uiLabels.midScoreHeading }} {{ questionNumber - 1 }}</h2>
+        <h3 class="top-participant">{{ topParticipantsText }}</h3>
         <div class="participant-list">
-            <div class="participant-item" v-for="person in participants" :key="person.name">
-                {{ person.name }}
-                <div>
-                    <img class="emojies" v-bind:src="person.avatar" target="_blank">
-                </div>
-                <div>
-                    {{ person.points }}
-                </div>
-            </div>
-            <div>
-                <img id="result-train" src="/img/resultTrain.png">
-            </div>
+            <ol>
+                <li v-for="(person, index) in participants" :key="person.name" class="participant-item">
+                    <div class="participant-info">
+                        <div class="name-avatar">
+                            {{ person.name }} <img class="emojies" v-bind:src="person.avatar" target="_blank">
+                        </div>
+                        <span class="points">{{ person.points }}</span>
+                    </div>
+                </li>
+            </ol>
         </div>
+        <div class="right-answer-box">
+        <p class="right-answer">{{ uiLabels.rightAnswerText }}:
+            {{ Object.keys(cities)[questionNumber - 2] }}
+        </p>
+    </div>
     </div>
 </template>
-
+  
 <script>
 export default {
     props: {
@@ -49,20 +51,66 @@ export default {
             } else {
                 return 'No participants';
             }
-        }
-
+        },
     }
 }
 </script>
   
 
 <style scoped>
+
+.right-answer,
+.top-participant {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color: black;
+    font-size: 2vw;
+}
+
+.participant-item {
+    padding: 1vw;
+    margin-bottom: 0.5vw;
+}
+
+.participant-item:nth-child(odd) {
+    background-color: rgb(140, 204, 140);
+}
+
+.participant-item:nth-child(even) {
+    background-color: #e3a5d3;
+}
+
 .score-board-component {
-    text-align: center;
-    margin: 20px;
-    padding: 10px;
-    background-color: green;
-    border-radius: 10px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    position: center;
+    padding: 10em auto 2em 2em;
+    font-size: 2vw;
+    width: 50vw;
+    height: 40vw;
+    background-size: cover;
+    /* border-radius: 2vw;
+    background-color: rgb(201, 241, 244);
+    border: 2px solid black; */
+    margin-left: 25vw;
+    margin-top: 3vw;
+}
+
+.participant-list {
+  padding: 10em auto 2em 2em;
+  margin-left: 1vw;
+  margin-right: 4vw;
+  text-align: left;
+  /* max-height: 20vw;
+  overflow-y: auto; */
+}
+
+.participant-item {
+    list-style-type: none;
+}
+
+.participant-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 
 .emojies {
@@ -70,17 +118,6 @@ export default {
     height: 2vw;
 }
 
-.participant-item {
-    display: inline-block;
-    justify-content: space-around;
-    /* Adjust as needed */
-    align-items: center;
-    margin: 0 10px;
-    /* Adjust margin as needed */
-}
-
-#result-train {
-    width: 100%;
-    height: 100%;
-}
-</style>
+.points {
+    text-align: right;
+}</style>
